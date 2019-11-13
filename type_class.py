c244b77be324a1724a3988a1c910c3ca89c75274
JMGallego <jmgallego@ZiMac4-Master.local>
2019-11-13 20:07:42 +0100

    exaxmple class framework

---
+++ type_class.py
@@ -0,0 +1,14 @@
+class UserRoutes(HeredarFramework):
+
+    def get('/users'):
+        pass
+
+class CertificaRoutes(HeredarFramework):
+
+    def post('/certificates'):
+        pass
+class AnotherRotues(HeredarFramework):
+
+    @post
+    def login('/whatever'):
+        pass
