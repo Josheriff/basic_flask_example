131b5f5e3a8af6334b60046d9ac06009f3f1a6b0
JMGallego <jmgallego@ZiMac4-Master.local>
2019-11-13 20:04:14 +0100

    just simple

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'GET':
+        greetings = {'text':'Hello world'}
+        return jsonify(greetings)
+    if request.method == 'POST':
+        data = request.json
+        age = data.get('age')
+        if age > 30:
+            return jsonify({'text':'Esto es una app para jovenes, ve a dar de comer a las palomas'})
+        return jsonify({'text':'bienvendio'})
+
+
+
+if __name__ == "__main__":
+    app.run()
