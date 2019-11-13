d60bc54ff4861c17caa79a9adf925c43393b88a5
JMGallego <jmgallego@ZiMac4-Master.local>
2019-11-13 21:27:41 +0100

    así se lo ponían a felipe II

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 from flask import Flask, request, jsonify
+from persistence import update_from_front
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/users', methods=['GET', 'POST'])
 def hello_world():
-    if request.method == 'GET':
-        greetings = {'text':'Hello world'}
-        return jsonify(greetings)
-        
+         
     if request.method == 'POST':
         data = request.json
-        age = data.get('age')
-        if age > 30:
-            return jsonify({'text':'Esto es una app para jovenes, ve a dar de comer a las palomas'})
-        return jsonify({'text':'bienvendio'})
+        update_from_front(data)
 
 
 
